a93789dd758a963d05ea26615de4365c71a41d13
Manik <manik@pixelconception.com>
2025-02-17 16:44:09 +0530

    Final code for TCC328

--- src/app.ts
+++ src/app.ts
@@ -1,22 +1,26 @@
+//! Basic Conditional Types
+type IsString<T> = T extends string ? "Yes" : "No";
+
+type Test1 = IsString<string>; // "Yes"
+type Test2 = IsString<number>; // "No"
+
+//! Creating Utility Types with Conditional Types
+type OptionalIfString<T> = {
+  [K in keyof T]: T[K] extends string ? T[K] | undefined : T[K];
+};
+
 type User = {
   id: number;
   name: string;
   age: number;
 };
 
-type UserNameType = User["name"]; // Extracts `string`
+type OptionalNameUser = OptionalIfString<User>;
 
-type UserIDType = User["id"]; // Extracts `number`
-
-const userName: UserNameType = "Alice"; // ✅ Allowed
-// const invalidUserName: UserNameType = 42; // ❌ Error: Type 'number' is not assignable to 'string'
-
-//! Using Indexed Access Types in Function
-function getProperty<T, K extends keyof T>(obj: T, key: K): T[K] {
-  return obj[key];
-}
-
-const userData: User = { id: 1, name: "Bob", age: 25 };
-
-const userName2: User["name"] = getProperty(userData, "name"); // ✅ Works fine
-const userAge: User["age"] = getProperty(userData, "age"); // ✅ Works fine
+//  Equivalent to:
+/* type OptionalNameUser = {
+  id: number;
+  name?: string;
+  age: number;
+};
+ */
