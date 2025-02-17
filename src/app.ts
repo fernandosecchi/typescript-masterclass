1825b4148e55c2d4c8f87d2ac1cb1254973752fc
Manik <manik@pixelconception.com>
2025-02-17 17:00:31 +0530

    Final code for TCC329

--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,4 @@
-//! Basic Conditional Types
-type IsString<T> = T extends string ? "Yes" : "No";
-
-type Test1 = IsString<string>; // "Yes"
-type Test2 = IsString<number>; // "No"
-
-//! Creating Utility Types with Conditional Types
-type OptionalIfString<T> = {
-  [K in keyof T]: T[K] extends string ? T[K] | undefined : T[K];
-};
+//! Making All Properties Optional using Mapped Types
 
 type User = {
   id: number;
@@ -15,12 +6,23 @@ type User = {
   age: number;
 };
 
-type OptionalNameUser = OptionalIfString<User>;
-
-//  Equivalent to:
-/* type OptionalNameUser = {
-  id: number;
-  name?: string;
-  age: number;
+type PartialUser = {
+  [K in keyof User]?: User[K];
 };
- */
+
+const user1: PartialUser = { name: "Alice" }; // ✅ Allowed
+// const user2: PartialUser = { unknownProp: 42 }; // ❌ Error: unknownProp does not exist
+
+//! Mapping Property Types Conditionally
+type OptionalIfString<T> = {
+  [K in keyof T]: T[K] extends string ? T[K] | undefined : T[K];
+};
+
+type UserWithOptionalStrings = OptionalIfString<User>;
+/* Equivalent to:
+  type UserWithOptionalStrings = {
+    id: number;
+    name?: string;
+    age: number;
+  }
+*/
