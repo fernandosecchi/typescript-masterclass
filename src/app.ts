5c29165c2e94eb2aab48d7988a46ca1465c7053e
Manik <manik@pixelconception.com>
2025-02-18 11:40:46 +0530

    Final code for TCC356

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,12 @@
-function methodLogger(originalMethod: any, _context: any) {
-  function replacementMethod(this: any, ...args: any[]) {
-    console.log("Invocation Started");
-    const result = originalMethod.call(this, ...args);
-    console.log("Invocation ended");
-    return result;
-  }
-  return replacementMethod;
+function methodLogger(logPrefix: string) {
+  return function (originalMethod: any, _context: any) {
+    return function (this: any, ...args: any[]) {
+      console.log(`${logPrefix}: Invocation Started`);
+      const result = originalMethod.call(this, ...args);
+      console.log(`${logPrefix}: Invocation ended`);
+      return result;
+    };
+  };
 }
 
 function bound(_originalMethod: any, context: any) {
@@ -24,7 +25,7 @@ class Person {
   constructor(public name: string) {}
 
   @bound
-  @methodLogger
+  @methodLogger("LOG")
   greet(greeting: string) {
     console.dir(this);
     console.log(` ${greeting}, ${this.name}`);
