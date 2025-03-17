f9b2a89a194ba48e8b9c784f6af60032d9504f2f
FAS <fernando@secchi.name>
2025-03-17 20:49:22 -0300

    segundo commit

--- src/domain/interfaces/i-aggregate-root.interface.ts
+++
@@ -1,6 +0,0 @@
-import { IEntity } from './i-entity.interface';
-
-export interface IAggregateRoot extends IEntity {
-    // Base interface for aggregate roots in our domain
-    // This helps identify which entities are aggregate roots
-}
